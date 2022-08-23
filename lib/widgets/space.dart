ed3b78489ada22d3382bbb0a4c85ac2315d82476
Timo Gruber <timo.dev.gruber@gmail.com>
2022-08-23 19:30:29 +0200

    updated space

--- lib/widgets/space.dart
+++ lib/widgets/space.dart
@@ -3,7 +3,7 @@
 part of wyd_core;
 
 class Space extends StatelessWidget {
-  Space({super.key, this.h, this.w});
+  Space({super.key, this.h = kSpacing.xs, this.w = kSpacing.xs});
 
   double? h;
   double? w;
@@ -14,5 +14,27 @@ class Space extends StatelessWidget {
       height: kSpacing.xs,
       width: w,
     );
+  }
+}
+
+class HSpace extends StatelessWidget {
+  HSpace({super.key, this.h = kSpacing.xs});
+
+  double? h;
+
+  @override
+  Widget build(BuildContext context) {
+    return Space(h: h);
+  }
+}
+
+class VSpace extends StatelessWidget {
+  VSpace({super.key, this.v = kSpacing.xs});
+
+  double? v;
+
+  @override
+  Widget build(BuildContext context) {
+    return Space(h: v);
   }
 }
