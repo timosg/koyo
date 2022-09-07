3f6e69d52ee4ee056e974c27b1d5ac0d9667dd4c
Timo Gruber <timo.dev.gruber@gmail.com>
2022-09-07 17:16:35 +0200

    updated

--- lib/extensions/color.dart
+++ lib/extensions/color.dart
@@ -6,13 +6,6 @@ extension ColorExtension on Color {
 
   bool get isLight => computeLuminance() > _luminanceTreshold;
 
-  static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: _hexCode));
-  }
-
   String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
       '${alpha.toRadixString(_hexCode).padLeft(2, '0')}'
       '${red.toRadixString(_hexCode).padLeft(2, '0')}'
