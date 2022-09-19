2f8057716d8472487c12b2e4bc18c596ddedd1d4
Timo Gruber <timo.dev.gruber@gmail.com>
2022-09-19 18:49:49 +0200

    updated

--- lib/src/widgets/simple_list.dart
+++ lib/src/widgets/simple_list.dart
@@ -7,39 +7,40 @@ class SimpleList<T> extends StatelessWidget {
     required this.builder,
     this.spacing,
     this.padding = EdgeInsets.zero,
-    this.axis = Axis.vertical,
+    this.scrollDirection = Axis.vertical,
   });
 
   final List<T> items;
   final Widget Function(BuildContext, T, int) builder;
   final double? spacing;
   final EdgeInsets padding;
-  final Axis axis;
+  final Axis scrollDirection;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: axis,
-      child: Padding(
-        padding: padding,
-        child: axis == Axis.vertical
-            ? Column(
+    return scrollDirection == Axis.vertical
+        ? SingleChildScrollView(
+            scrollDirection: scrollDirection,
+            child: Padding(
+              padding: padding,
+              child: Column(
                 children: <Widget>[
                   for (int index = 0; index < items.length; index++) ...[
                     if (index != 0) HSpace(h: spacing),
                     builder(context, items[index], index),
                   ]
                 ],
-              )
-            : Row(
-                children: <Widget>[
-                  for (int index = 0; index < items.length; index++) ...[
-                    if (index != 0) WSpace(w: spacing),
-                    builder(context, items[index], index),
-                  ]
-                ],
               ),
-      ),
-    );
+            ),
+          )
+        : ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              for (int index = 0; index < items.length; index++) ...[
+                if (index != 0) WSpace(w: spacing),
+                builder(context, items[index], index),
+              ]
+            ],
+          );
   }
 }
