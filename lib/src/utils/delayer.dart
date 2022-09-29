5f9ebd96a0057582fb90f92f2eb8a250013d37b3
Timo Gruber <timo.dev.gruber@gmail.com>
2022-09-29 19:41:32 +0200

    updated

--- lib/src/utils/delayer.dart
+++ lib/src/utils/delayer.dart
@@ -3,6 +3,11 @@ import 'package:flutter/scheduler.dart';
 class Delayer {
   const Delayer();
 
-  void post(void Function() callback) => SchedulerBinding.instance
-      .addPostFrameCallback((Duration _) => callback());
+  void postFrame(void Function() callback) {
+    SchedulerBinding.instance.addPostFrameCallback((Duration _) => callback());
+  }
+
+  void delay(Duration duration, void Function() callback) {
+    Future.delayed(duration, callback);
+  }
 }
