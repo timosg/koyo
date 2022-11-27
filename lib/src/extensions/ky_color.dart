380e2270d24f02688caefc54bc6e287d5b35175a
Timo Gruber <timo.dev.gruber@gmail.com>
2022-11-27 12:20:03 +0100

    updated docs

--- lib/src/extensions/ky_color.dart
+++ lib/src/extensions/ky_color.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 extension KyColorExtension on Color {
+  /// [_luminanceTreshold] constant value to calculate which color is Dark/Light
   static const _luminanceTreshold = 0.179;
-  static const _hexCode = 16;
 
+  /// Check if [computeLuminance()] is bigger than [_luminanceTreshold]
   bool get isLight => computeLuminance() > _luminanceTreshold;
 
+  /// Calculator Color [toHex]
   String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-      '${alpha.toRadixString(_hexCode).padLeft(2, '0')}'
-      '${red.toRadixString(_hexCode).padLeft(2, '0')}'
-      '${green.toRadixString(_hexCode).padLeft(2, '0')}'
-      '${blue.toRadixString(_hexCode).padLeft(2, '0')}';
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
